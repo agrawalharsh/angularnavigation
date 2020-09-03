4d1c13447576d1a5a5ef794befdcc9913d6d74d2
myusername <myemail>
2020-09-03 15:23:52 +0530

    routing basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
 
 
 const routes: Routes = [
-  { path: 'first-component', component: FirstComponent,
-    children: [
-      {
-        path: 'child-a', // child route path
-        component: ChildAComponent, // child route component that the router renders
-      },
-      {
-        path: 'child-b',
-        component: ChildBComponent, // another child route component that the router renders
-      },
-      ]
-  },
-  { path: 'second-component', component: SecondComponent },
-  { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+
 ];
 
 @NgModule({
